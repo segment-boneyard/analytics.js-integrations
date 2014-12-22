2293bda41b905f080fcadd37e2fca7708c705acd
Adam Marcus <marcua@marcua.net>
2014-12-22 09:41:47 -0500

    Add kronos.js support

--- test/index.js
+++ test/index.js
@@ -21,7 +21,8 @@ mocha.setup({
 
 describe('integrations', function(){
   it('should export our integrations', function(){
-    assert.equal(80, object.length(Integrations));
+    assert.equal(81, object.length(Integrations));
+    assert.equal(75, object.length(Integrations));
   });
 });
 
